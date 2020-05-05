c66c85ea7c75acaee02fbde8429541a6454f0494
tashmanr <rebeccatashman@gmail.com>
2020-05-05 11:45:49 +0300

    new CollisionInfo constructor to receive line and collidables

--- Ass3/src/CollisionInfo.java
+++ Ass3/src/CollisionInfo.java
@@ -1,3 +1,5 @@
+import java.util.List;
+
 /**
  * 336423124
  * Rebecca Tashman
@@ -7,18 +9,42 @@ public class CollisionInfo {
     private Point point;
     private Collidable collidable;
 
-    public CollisionInfo(Point collisionPoint, Collidable collisionObject){
+    public CollisionInfo(Point collisionPoint, Collidable collisionObject) {
         this.point = collisionPoint;
         this.collidable = collisionObject;
     }
 
+    public CollisionInfo(Line trajectory, List<Collidable> collidables) {
+        Point closest = null;
+        double distance = 0;
+        Collidable c2 = null;
+        for (Collidable c : collidables) {
+            Rectangle r = c.getCollisionRectangle();
+            if (r.intersectionPoints(trajectory).size() == 0) {
+                continue;
+            }
+            Point tmp = trajectory.closestIntersectionToStartOfLine(r);
+            if (closest == null) {
+                closest = tmp;
+                distance = trajectory.start().distance(closest);
+                c2 = c;
+            } else if (trajectory.start().distance(tmp) < distance) {
+                closest = tmp;
+                distance = trajectory.start().distance(closest);
+                c2 = c;
+            }
+        }
+        this.point = closest;
+        this.collidable = c2;
+    }
+
     // the point at which the collision occurs.
-    public Point collisionPoint(){
+    public Point collisionPoint() {
         return this.point;
     }
 
     // the collidable object involved in the collision.
-    public Collidable collisionObject(){
+    public Collidable collisionObject() {
         return this.collidable;
     }
 }
