be8774df9b1b39c2c13a7a8e2b402a89a520f974
tashmanr <rebeccatashman@gmail.com>
2020-05-05 09:05:54 +0300

    added SpriteCollection class

---
+++ Ass3/src/SpriteCollection.java
@@ -0,0 +1,31 @@
+/**
+ * 336423124
+ * Rebecca Tashman
+ */
+
+import java.util.ArrayList;
+import java.util.List;
+
+import biuoop.DrawSurface;
+
+public class SpriteCollection {
+    private List<Sprite> sprites = new ArrayList<Sprite>();
+
+    public void addSprite(Sprite s) {
+        sprites.add(s);
+    }
+
+    // call timePassed() on all sprites.
+    public void notifyAllTimePassed() {
+        for (Sprite s : sprites) {
+            s.timePassed();
+        }
+    }
+
+    // call drawOn(d) on all sprites.
+    public void drawAllOn(DrawSurface d) {
+        for (Sprite s : sprites) {
+            s.drawOn(d);
+        }
+    }
+}
