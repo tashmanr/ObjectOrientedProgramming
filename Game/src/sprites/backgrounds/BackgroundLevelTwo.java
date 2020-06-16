315162dd32b5658d9211d3a90465b719323ad900
tashmanr <rebeccatashman@gmail.com>
2020-06-16 15:52:28 +0300

    completed BackgroundLevelTwo

--- Game/src/sprites/backgrounds/BackgroundLevelTwo.java
+++ Game/src/sprites/backgrounds/BackgroundLevelTwo.java
@@ -15,9 +15,12 @@ public class BackgroundLevelTwo implements Sprite {
     public void drawOn(DrawSurface d) {
         d.setColor(Color.white);
         d.fillRectangle(0,0, 800, 600);
-        d.setColor(new Color(255,255,153)); // outer circle light yellow
+        d.setColor(new Color(237, 237, 129)); // outer circle light yellow
         d.fillCircle(150, 150, 60);
-        d.setColor(new Color(255,204,0)); // mid circle dark yellow
+        for (int i = 0; i < 700; i+= 5) {
+            d.drawLine(150, 150, i, 250);
+        }
+        d.setColor(new Color(221, 219, 81)); // mid circle dark yellow
         d.fillCircle(150, 150, 50);
         d.setColor(Color.yellow); // inner circle yellow
         d.fillCircle(150, 150, 40);
